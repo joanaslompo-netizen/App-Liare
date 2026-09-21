5146e240594ee17ddd0ece21720dbbcd056ad32c
joanaslompo-netizen <joanaslompo@gmail.com>
2026-09-21 07:57:11 -0300

    Trigger production redeploy for updated home banner

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -36,6 +36,7 @@ interface HomeViewProps {
   onOpenSettings?: () => void;
 }
 
+// Redeploy marker: summary banner
 export const HomeView: React.FC<HomeViewProps> = ({
   artisanName,
   atelierName,
